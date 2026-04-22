cc3b594a90b15a0141e5700022c1708ab376faba
Bruno Dias <bruno.dias1893@gmail.com>
2026-04-22 02:19:03 +0100

    1

--- src/components/MovieList/index.tsx
+++ src/components/MovieList/index.tsx
@@ -10,22 +10,27 @@ import { ClipLoader } from "react-spinners";
 export default function MovieList (){
 
     const [movies, setMovies] = useState<Movie[]>([]);
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [totalPages, setTotalPages] = useState<number>(0);
     const [isLoading, setIsLoading] = useState<boolean>(true);
 
     useEffect(() => {
-        getMovies();
-    }, [])
+        getMovies(currentPage); //pass page number to getMovies function
+    }, [currentPage]); //fetch movies when currentPage changes
 
-    const getMovies = async () =>{ 
+    const getMovies = async (page: number) =>{ 
+        setIsLoading(true); //set loading state to true before fetching data
         await axios({
             method: "get",
             url: "https://api.themoviedb.org/3/discover/movie",
             params: {
                 api_key: "9ab0c1b5c24de8fee8cff270d3f18e70",
                 language: "en-US",
+                page, //pass page number as a parameter
             }
         }).then(response => {            
             setMovies(response.data.results);
+            setTotalPages(response.data.total_pages);
             console.log(response.data.results);
         });
 
@@ -41,6 +46,7 @@ export default function MovieList (){
     }
 
     return (
+        <>
             <ul className="movie-list">
                 {movies.map((movie) => 
                     <MovieCard
@@ -49,6 +55,27 @@ export default function MovieList (){
                     />
                 )}
             </ul>
+
+            <div className="pagination">
+                <button
+                onClick={() => setCurrentPage((p) => p - 1)}
+                disabled={currentPage === 1}
+                >
+                    
+                ← Prev
+                </button>
+
+                <span>{currentPage} out of {totalPages}</span>
+
+                <button
+                    onClick={() => setCurrentPage((p) => p + 1)}
+                    disabled={currentPage === totalPages}
+                    >
+
+                    Next →
+                </button>
+            </div>
+        </>
     )
 }
 
