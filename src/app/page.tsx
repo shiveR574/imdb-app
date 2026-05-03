fa3397d6b8de14bb43569150b23223b0ab6f9a08
Bruno Dias <bruno.dias1893@gmail.com>
2026-05-03 23:40:53 +0100

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ export default function HomePage() {
 
   return (
   <div className="home-page-title">
-    <p className="paragraph1">Welcome to IMDB!</p>
+    {/* <p className="paragraph1">Welcome to IMDB!</p> */}
     {/* <p>To discover the best movies and TV shows, click on the links above or just search on the search bar</p> */}
     
     <div className="movies-container">
