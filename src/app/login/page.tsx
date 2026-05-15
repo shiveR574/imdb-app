08d5c99b74627914fb9533d1cd399f226d6ac98a
Bruno Dias <bruno.dias1893@gmail.com>
2026-05-15 01:34:11 +0100

    1

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,8 +2,23 @@ import "../login/page.scss";
 
 export default function Login () {
     return (
-        <div>
-            Login
+        <div className="login-container">
+            <div className="form-container">
+                <h1 className="form-title">Login</h1>
+                <form>
+                    <input type="text" 
+                    className="email-input" 
+                    placeholder="Email" 
+                    required
+                    />
+                    <input type="password" 
+                    className="password-input" 
+                    placeholder="Password" 
+                    required
+                    />
+                    <button type="submit" className="btn">Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,8 +2,23 @@ import "../register/page.scss";
 
 export default function Register () {
     return (
-        <div>
-            Register
+        <div className="register-container">
+            <div className="form-container">
+                <h1 className="form-title">Register</h1>
+                <form>
+                    <input type="text" 
+                    className="email-input" 
+                    placeholder="Email" 
+                    required
+                    />
+                    <input type="password" 
+                    className="password-input" 
+                    placeholder="Password" 
+                    required
+                    />
+                    <button type="submit" className="btn">Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
