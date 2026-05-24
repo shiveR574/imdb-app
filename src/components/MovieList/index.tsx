b7f648492cf7e75c2eeb004daf3a56059c97b8a9
Bruno Dias <bruno.dias1893@gmail.com>
2026-05-24 03:17:29 +0100

    1

--- src/components/MovieList/index.tsx
+++ src/components/MovieList/index.tsx
@@ -5,14 +5,18 @@ import "./index.scss";
 import MovieCard from "../MovieCard";
 import { Movie } from "../../types/movie";
 import { ClipLoader } from "react-spinners";
+import {useSearchParams, useRouter} from "next/navigation";
 
 
 export default function MovieList (){
 
     const [movies, setMovies] = useState<Movie[]>([]);
-    const [currentPage, setCurrentPage] = useState<number>(1);
     const [totalPages, setTotalPages] = useState<number>(0);
     const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    const searchParams = useSearchParams();
+    const router = useRouter();
+    const currentPage = Number(searchParams.get("page")) || 1; // reading the page from URL
 
     const TMDB_API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 
@@ -42,6 +46,10 @@ export default function MovieList (){
         }
     }
 
+    const handlePageChange = (page: number) => {
+        router.push(`/movies?page=${page}`); //saving page here in URL
+    }
+
     if (isLoading) {
         return (
             <div className="loading-container">
@@ -63,7 +71,7 @@ export default function MovieList (){
 
             <div className="pagination">
                 <button
-                onClick={() => setCurrentPage((p) => p - 1)}
+                onClick={() => handlePageChange(currentPage - 1)}
                 disabled={currentPage === 1}
                 >
                     
@@ -73,7 +81,7 @@ export default function MovieList (){
                 <span>{currentPage} out of {totalPages}</span>
 
                 <button
-                    onClick={() => setCurrentPage((p) => p + 1)}
+                    onClick={() => handlePageChange(currentPage + 1)}
                     disabled={currentPage === totalPages}
                     >
 
